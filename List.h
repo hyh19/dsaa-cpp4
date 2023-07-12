94ce78a33c5c12a18a82c6c6b553c16d910586d9
e7e26e5 <abc@xyz.com>
2023-07-12 12:10:39 +0800

    refactor

--- List.h
+++ List.h
@@ -26,40 +26,45 @@ private:
 public:
     class const_iterator {
     public:
-
         // Public constructor for const_iterator.
         const_iterator() : current{nullptr} {}
 
         // Return the object stored at the current position.
         // For const_iterator, this is an accessor with a
         // const reference return type.
-        const Object &operator*() const { return retrieve(); }
+        virtual const Object &operator*() const {
+            return retrieve();
+        }
 
-        const_iterator &operator++() {
+        virtual const_iterator &operator++() {
             current = current->next;
             return *this;
         }
 
-        const_iterator operator++(int) {
+        virtual const_iterator operator++(int) {
             const_iterator old = *this;
             ++(*this);
             return old;
         }
 
-        const_iterator &operator--() {
+        virtual const_iterator &operator--() {
             current = current->prev;
             return *this;
         }
 
-        const_iterator operator--(int) {
+        virtual const_iterator operator--(int) {
             const_iterator old = *this;
             --(*this);
             return old;
         }
 
-        bool operator==(const const_iterator &rhs) const { return current == rhs.current; }
+        bool operator==(const const_iterator &rhs) const {
+            return current == rhs.current;
+        }
 
-        bool operator!=(const const_iterator &rhs) const { return !(*this == rhs); }
+        bool operator!=(const const_iterator &rhs) const {
+            return !(*this == rhs);
+        }
 
     protected:
         Node *current;
@@ -67,7 +72,9 @@ public:
         // Protected helper in const_iterator that returns the object
         // stored at the current position. Can be called by all
         // three versions of operator* without any type conversions.
-        Object &retrieve() const { return current->data; }
+        Object &retrieve() const {
+            return current->data;
+        }
 
         // Protected constructor for const_iterator.
         // Expects a pointer that represents the current position.
@@ -86,13 +93,17 @@ public:
         // would be disabled.
         iterator() = default;
 
-        Object &operator*() { return const_iterator::retrieve(); }
+        Object &operator*() {
+            return const_iterator::retrieve();
+        }
 
         // Return the object stored at the current position.
         // For iterator, there is an accessor with a
         // const reference return type and a mutator with
         // a reference return type. The accessor is shown first.
-        const Object &operator*() const { return const_iterator::operator*(); }
+        const Object &operator*() const {
+            return const_iterator::operator*();
+        }
 
         iterator &operator++() {
             this->current = this->current->next;
@@ -125,7 +136,9 @@ public:
     };
 
 public:
-    List() { init(); }
+    List() {
+        init();
+    }
 
     ~List() {
         clear();
@@ -135,8 +148,9 @@ public:
 
     List(const List &rhs) {
         init();
-        for (auto &x: rhs)
+        for (auto &x: rhs) {
             push_back(x);
+        }
     }
 
     List &operator=(const List &rhs) {
@@ -163,48 +177,81 @@ public:
 
     // Return iterator representing beginning of list.
     // Mutator version is first, then accessor version.
-    iterator begin() { return iterator(head->next); }
+    iterator begin() {
+        return iterator(head->next);
+    }
 
-    const_iterator begin() const { return const_iterator(head->next); }
+    const_iterator begin() const {
+        return const_iterator(head->next);
+    }
 
     // Return iterator representing endmarker of list.
     // Mutator version is first, then accessor version.
-    iterator end() { return iterator(tail); }
+    iterator end() {
+        return iterator(tail);
+    }
 
-    const_iterator end() const { return const_iterator(tail); }
+    const_iterator end() const {
+        return const_iterator(tail);
+    }
 
     // Return number of elements currently in the list.
-    int size() const { return theSize; }
+    int size() const {
+        return theSize;
+    }
 
     // Return true if the list is empty, false otherwise.
-    bool empty() const { return size() == 0; }
+    bool empty() const {
+        return size() == 0;
+    }
 
     void clear() {
-        while (!empty())
+        while (!empty()) {
             pop_front();
+        }
     }
 
     // front, back, push_front, push_back, pop_front, and pop_back
     // are the basic double-ended queue operations.
-    Object &front() { return *begin(); }
+    Object &front() {
+        return *begin();
+    }
 
-    const Object &front() const { return *begin(); }
+    const Object &front() const {
+        return *begin();
+    }
 
-    Object &back() { return *--end(); }
+    Object &back() {
+        return *--end();
+    }
 
-    const Object &back() const { return *--end(); }
+    const Object &back() const {
+        return *--end();
+    }
 
-    void push_front(const Object &x) { insert(begin(), x); }
+    void push_front(const Object &x) {
+        insert(begin(), x);
+    }
 
-    void push_back(const Object &x) { insert(end(), x); }
+    void push_back(const Object &x) {
+        insert(end(), x);
+    }
 
-    void push_front(Object &&x) { insert(begin(), std::move(x)); }
+    void push_front(Object &&x) {
+        insert(begin(), std::move(x));
+    }
 
-    void push_back(Object &&x) { insert(end(), std::move(x)); }
+    void push_back(Object &&x) {
+        insert(end(), std::move(x));
+    }
 
-    void pop_front() { erase(begin()); }
+    void pop_front() {
+        erase(begin());
+    }
 
-    void pop_back() { erase(--end()); }
+    void pop_back() {
+        erase(--end());
+    }
 
     // Insert x before itr.
     iterator insert(iterator itr, const Object &x) {
@@ -233,14 +280,15 @@ public:
     }
 
     iterator erase(iterator from, iterator to) {
-        for (iterator itr = from; itr != to;)
+        for (iterator itr = from; itr != to;) {
             itr = erase(itr);
+        }
 
         return to;
     }
 
 private:
-    int theSize;
+    int theSize{};
     Node *head;
     Node *tail;
 
